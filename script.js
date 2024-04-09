1a665455cb6cdc1ee64c59bdacce9ad3dcbb891f
Ye Myint Kyaw <yemyintkyaw241220@gmail.com>
2024-04-09 08:43:46 +0630

    add GSAP Scroll Trigger and Lenis

--- script.js
+++ script.js
@@ -1,36 +1,24 @@
 const targetSection = document.querySelectorAll('.section');
-const fixedText = document.querySelector(".kv__text");
+// const fixedText = document.querySelector(".kv__text");
 
-targetSection.forEach(section=>{
-    const rect = section.getBoundingClientRect();
-    if (rect.top >= 0 && (rect.top+200) <= window.innerHeight) {
-        section.classList.add('is-act');
-    }
-})
-const rectText = targetSection[0].getBoundingClientRect();
-if(rectText.top < 0){
-    fixedText.classList.add("is-change");
-    fixedText.classList.add("is-act");
-}else if(rectText.top - fixedText.offsetHeight < 0){
-    fixedText.classList.add("is-act");
-}else{
-    fixedText.classList.remove("is-act");
-    fixedText.classList.remove("is-change");
-}
+// targetSection.forEach(section=>{
+//     const rect = section.getBoundingClientRect();
+//     if (rect.top >= 0 && (rect.top+200) <= window.innerHeight) {
+//         section.classList.add('is-act');
+//     }
+// })
+// const rectText = targetSection[0].getBoundingClientRect();
+// if(rectText.top < 0){
+//     fixedText.classList.add("is-change");
+//     fixedText.classList.add("is-act");
+// }else if(rectText.top - fixedText.offsetHeight < 0){
+//     fixedText.classList.add("is-act");
+// }else{
+//     fixedText.classList.remove("is-act");
+//     fixedText.classList.remove("is-change");
+// }
 
 window.addEventListener('scroll', function() {
-
-    const rectText = targetSection[0].getBoundingClientRect();
-    if(rectText.top < 0){
-        fixedText.classList.add("is-change");
-    }else if(rectText.top - fixedText.offsetHeight < 0){
-        fixedText.classList.add("is-act");
-    }else{
-        fixedText.classList.remove("is-act");
-        fixedText.classList.remove("is-change");
-    }
-
-
     targetSection.forEach(section=>{
         const rect = section.getBoundingClientRect();
         // Check if the top of the section is within the viewport
@@ -38,6 +26,47 @@ window.addEventListener('scroll', function() {
             section.classList.add('is-act');
         }
     })
-
-    
 });
+
+const paths = gsap.utils.toArray(".section path");
+
+paths.forEach(path=>{
+    const tl = gsap.timeline({
+        scrollTrigger:{
+            trigger:path,
+            scrub:true,
+            start:"center bottom"
+        }
+    });
+    tl.fromTo(path,
+        {
+            strokeDasharray: 1500,
+            strokeDashoffset: 1500
+        },
+        {
+            strokeDasharray: 1500,
+            strokeDashoffset: 0
+        },
+    )
+})
+
+
+// Smooth scroll
+const lenis = new Lenis(
+    {
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+    }
+  )
+  
+  lenis.on('scroll', (e) => {
+    console.log(e)
+  })
+  
+  function raf(time) {
+    lenis.raf(time);
+    ScrollTrigger.update();
+    requestAnimationFrame(raf);
+  }
+  
+  requestAnimationFrame(raf)
